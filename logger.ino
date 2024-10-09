384a67d0ec5628659375239ba394a14254a7a404
john brewster <bjohn316er@gmail.com>
2024-10-09 12:53:07 -0400

    init

---
+++ logger.ino
@@ -0,0 +1,86 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <SD.h>
+
+#define chipSelect 4
+
+File myFile;
+
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
+#define LED 0
+unsigned long delayTime;
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(LED, OUTPUT);
+    
+    Serial.begin(9600);
+    while(!Serial);    // time to get serial running
+    if (!SD.begin(chipSelect)) {
+      Serial.println("initialization failed.");
+    }
+    Serial.println(F("BME280 test"));
+
+
+    myFile = SD.open("test.txt", FILE_WRITE);
+
+
+    unsigned status;
+    
+    // default settings
+    status = bme.begin();  
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+        Serial.print("        ID of 0x60 represents a BME 280.\n");
+        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        while (1) delay(10);
+    }
+    
+    Serial.println("-- Default Test --");
+    delayTime = 1000;
+
+    Serial.println();
+    digitalWrite(LED, HIGH);
+
+}
+
+void loop() {
+    printValues();
+    delay(delayTime);
+}
+
+
+void printValues() {
+
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" °C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+
+}
